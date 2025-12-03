4bf552dd5bb53385144250cdae43274e17b4a10d
DolphenA <shroogali25@gmail.com>
2025-12-03 13:30:57 +0300

    Fix fireworks.js path to work on cloud

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/dashboard/Achiever/public/css/site.css">
-    <script src="/dashboard/Achiever/public/js/fireworks.js"></script>
+    <script src="{{ asset('js/fireworks.js') }}"></script>
     @yield('extra_css')
 </head>
 <body>
